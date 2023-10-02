8261421a32c068ca3c05b1aaffbe91d7a2876355
CodeSandbox <support@codesandbox.io>
2023-10-02 04:43:57 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
+import "./App.css";
 import { useState } from "react";
 import { useRef } from "react";
 function App() {
   const plus = () => {
     setResult(result + Number(inputRef.current.value));
   };
+  const minus = () => {
+    setResult(result - Number(inputRef.current.value));
+  };
+  const multiply = (e) => {
+    setResult(result * Number(inputRef.current.value));
+  };
+  const divide = () => {  
+    setResult(result / Number(inputRef.current.value));
+  };
+  const reset = () => {
+    setResult((prev) => prev * 0);
+  };
+  const resetInput = (e) => {
+    e.preventDefault;
+    inputRef.current.value = 0;
+  };
   const [result, setResult] = useState(0);
   const inputRef = useRef(null);
+  const resultRef = useRef(null);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <p>{result}</p>
-      <input ref={inputRef} placeholder="type your name" /> <br />
+      <h1>A SIMPLE ARITHMETIC CALCULATOR</h1>
+      <p ref={resultRef}>
+        <b>RESULT:</b> {result}
+      </p>
+      <input ref={inputRef} placeholder="input the number" /> <br />
       <button onClick={plus}>ADD</button>
+      <button onClick={minus}>MINUS</button>
+      <button onClick={multiply}>MULTIPLY</button>
+      <button onClick={divide}>DIVIDE</button>
+      <button onClick={reset}>Reset Result</button>
+      <button onClick={resetInput}>Reset Input</button>
     </div>
   );
 }
